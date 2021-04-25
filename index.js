c47e6567e5bca05025e78acad0d651fb3e9f0c02
Daniel Imhoff <dwieeb@gmail.com>
2021-04-25 14:49:40 -0700

    use 0 and 1 for false and true
    https://twitter.com/bongofortwo/status/1384007287912628225

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = l => {
-  var c, i = 0, s = '', r = [], q = false, e = false
+  var c, i = 0, s = '', r = [], q = 0, e = 0
   if (l.charCodeAt(i) === 0xfeff) i++
   while (c = l.charAt(i++)) {
     if (c === ',' && !q) { r.push(s); s = '' }
-    else if (c === '\\') { e = true }
-    else if (c === '"') { if (e) { s += c; e = false } else { q = !q } }
-    else if (e) { e = false }
+    else if (c === '\\') { e = 1 }
+    else if (c === '"') { if (e) { s += c; e = 0 } else { q = !q } }
+    else if (e) { e = 0 }
     else { s += c }
   }
   r.push(s)
